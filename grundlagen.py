20ccdbd830888b88755395e18b3ea70c9eb3c4ca
sarkb02 <sarkoziboroka@gmail.com>
2025-01-03 23:16:40 +0100

    Update grundlagen.py
    Zusatzübungen

--- grundlagen.py
+++ grundlagen.py
@@ -1,6 +1,8 @@
-# projektname = input("Wie heißt du?")
-# zusatz = " - digital"
-# print(projektname+zusatz)
+# Code aus der Übung
+
+projektname = input("Wie heißt du?")
+zusatz = " - digital"
+print(projektname+zusatz)
 
 
 k = int(input("Wie viele Tassen Kaffee hattest du?"))
@@ -18,3 +20,52 @@ while n < 8:
 
 else:
     print("Das war zu viel.")
+
+# Zusätzliche Übungen
+#1
+n = 1
+while n <= 100:
+  print(n)
+  n = n + 1 # n += 1
+
+#2
+n = 1
+while n <= 100:
+    if n % 3 == 0:
+        print("Digital")
+    else:
+        print(n)
+    n = n + 1 # n += 1
+
+#3
+n = 1
+while n <= 100:
+    if n % 5 == 0:
+        print("History")
+    else:
+        print(n)
+    n = n + 1 # n += 1
+
+#4
+n = 1
+while n <= 100:
+    if n % 5 == 0:
+        print("History")
+    elif n % 3 == 0:
+        print("Digital")
+    else:
+        print(n)
+    n = n + 1 # n += 1
+
+#5
+n = 1
+while n <= 100:
+    if (n % 3 == 0) & (n % 5 == 0):
+        print("Digital History")
+    elif n % 5 == 0:
+        print("History")
+    elif n % 3 == 0:
+        print("Digital")
+    else:
+        print(n)
+    n = n + 1 # n += 1
